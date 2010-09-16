2d8ed6263f252edf44e101193a8eadaa5746bf5b
Aleksandar Prokopec <aleksandar.prokopec@gmail.com>
2010-09-16 16:49:36 +0200

    Defined project file.

--- project/build/ThreadsProject.scala
+++ project/build/ThreadsProject.scala
@@ -1,15 +1,41 @@
 
-import sbt._
 
+import sbt._
+import Process._
 
 
 
 class ThreadsProject(info: ProjectInfo) extends DefaultProject(info) {
   
+  val ps = java.io.File.pathSeparator
+  val projName = "threads"
+  val scalaVersion = "2.8.0"
+  val projVersion = "1.0"
+  val bootDir = "project" / "boot"
+  val bootScalaDir = bootDir / ("scala-" + scalaVersion)
+  val bootLibDir = bootScalaDir / "lib"
+  val artifactName = projName + "_" + scalaVersion + "-" + projVersion + ".jar"
+  val classpath = List(
+    bootLibDir / "scala-library.jar",
+    "target" / ("scala_" + scalaVersion) / artifactName
+  ).mkString(ps)
   
   // actions
-  lazy val runserver = task {
-    "ls" !
+  
+  def javavm(settings: String, cp: String, mainclass: String) = "java " + settings + " -cp " + cp + " " + mainclass
+  
+  def runcommand(command: String) = {
+    println("Running: " + command)
+    command !
+  }
+  
+  lazy val server = task {
+    runcommand(javavm("-Xms256m -Xmx512m -server ", classpath, "scala.threads.app"))
+    None
+  }
+  
+  lazy val servergc = task {
+    runcommand(javavm("-Xms256m -Xmx512m -server ", classpath, "scala.threads.app"))
   }
   
 }
